f597d504a96cfc8e766a57d1308107fcf7624e19
AlexeyQQQ <leadterrier@gmail.com>
2024-01-24 15:14:10 +0300

    task_007 solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,32 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var titleTextView: TextView
+    private lateinit var changeButton: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sharedPreferences = getPreferences(Context.MODE_PRIVATE)
+        val saveText = sharedPreferences.getString("save_text", getString(R.string.hello_world))
+
+        titleTextView = findViewById(R.id.titleTextView)
+        changeButton = findViewById(R.id.changeButton)
+
+        titleTextView.text = saveText
+
+        changeButton.setOnClickListener {
+            titleTextView.text = getString(R.string.i_am_an_android_developer)
+            sharedPreferences.edit()
+                .putString("save_text", titleTextView.text.toString())
+                .apply()
+        }
     }
 }
