9403a2efe3d028527f04a9b3d3294fd8b4824de5
AlexeyQQQ <leadterrier@gmail.com>
2024-01-25 13:16:36 +0300

    task_009 solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,44 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.contains
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var titleTextView: TextView
+    private lateinit var hideButton: Button
+    private lateinit var rootLayout: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        titleTextView = findViewById(R.id.titleTextView)
+        hideButton = findViewById(R.id.removeButton)
+        rootLayout = findViewById(R.id.rootLayout)
+
+        hideButton.setOnClickListener {
+            rootLayout.removeView(titleTextView)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean(KEY_TITLE, !rootLayout.contains(titleTextView))
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState.getBoolean(KEY_TITLE)) {
+            rootLayout.removeView(titleTextView)
+        }
+    }
+
+    companion object {
+        private const val KEY_TITLE = "title_does_not_exist"
     }
 }
