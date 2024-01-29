e29dc856382c19c790dd4745ac3d1aee5713f47e
AlexeyQQQ <leadterrier@gmail.com>
2024-01-29 16:01:22 +0300

    task_014 solved (binding + handler)

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,38 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val handler = Handler(Looper.getMainLooper())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.actionButton.setOnClickListener {
+            load()
+        }
+    }
+
+    private fun load() {
+        binding.actionButton.isEnabled = false
+        binding.progressBar.visibility = View.VISIBLE
+
+        Thread {
+            Thread.sleep(2000)
+            handler.post {
+                binding.titleTextView.visibility = View.VISIBLE
+                binding.progressBar.visibility = View.INVISIBLE
+                binding.actionButton.isEnabled = true
+            }
+        }.start()
     }
 }
