1d4f8772dddc82c520fb95e0cc7217965fe67c94
AlexeyQQQ <leadterrier@gmail.com>
2024-01-30 13:13:49 +0300

    task_015 solved (MVVM + wrapper + repository)

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,32 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+
+    private val mainViewModelFactory = MainViewModelFactory(
+        LiveDataWrapper.Base(),
+        Repository.Base()
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this, mainViewModelFactory)[MainViewModel::class.java]
+        viewModel.liveData().observe(this) {
+            it.apply(binding.actionButton, binding.progressBar, binding.titleTextView)
+        }
+
+        binding.actionButton.setOnClickListener {
+            viewModel.load()
+        }
     }
 }
