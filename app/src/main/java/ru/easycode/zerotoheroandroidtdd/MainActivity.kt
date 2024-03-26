2e5752faba9a9eec7f816e67405ae65be7a60469
AlexeyQQQ <leadterrier@gmail.com>
2024-03-26 16:22:03 +0300

    task_020 solved (inputEditText)

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,23 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.actionButton.setOnClickListener {
+            val text = binding.inputEditText.text.toString()
+            binding.titleTextView.text = text
+            binding.inputEditText.text?.clear()
+        }
     }
 }
